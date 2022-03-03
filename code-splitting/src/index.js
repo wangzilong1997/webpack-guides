cb5985fb676d940024e558f6378f19eb0384f9e4
wangzilong1997 <1321787581@qq.com>
2022-03-03 16:50:19 +0800

    :green_heart: 代码分离-动态导入

--- code-splitting/src/index.js
+++ code-splitting/src/index.js
@@ -1,13 +1,10 @@
-import _ from 'lodash'
-
-function component() {
+async function getComponent() {
   const element = document.createElement('div');
-
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // lodash 在当前 script 中使用 import 引入
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+  const { default: _ } = await import('lodash')
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  return element
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component)
+})
