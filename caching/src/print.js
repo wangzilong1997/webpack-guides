22d64a39c9abe3fd235e3ac22518690096ab4e12
wangzilong1997 <1321787581@qq.com>
2022-03-04 11:18:15 +0800

    :racehorse: 缓存

---
+++ caching/src/print.js
@@ -0,0 +1,5 @@
+export default function print(text) {
+  console.log(text)
+  console.log('hello 666')
+  console.log('hello')
+}
---
+++ caching/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Caching'
+    })
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        }
+      }
+    }
+  }
+};
